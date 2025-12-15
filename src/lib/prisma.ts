dcf08ab2473d24bf9d215266b59f4589074ff6c5
Adam J. <mohamed.adam.jemal@gmail.com>
2025-12-15 13:15:22 +0100

    deployement fixes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,9 +3,10 @@ import { PrismaClient } from '@prisma/client';
 const globalForPrisma = global as any;
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
+  globalForPrisma.prisma ||  // Use existing PrismaClient if it exists
+  new PrismaClient({          // Otherwise, create a new one
     log: process.env.NODE_ENV === 'development' ? ['query'] : [],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') 
+  globalForPrisma.prisma = prisma; // Cache the client for hot reload in dev
